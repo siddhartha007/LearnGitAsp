b4e8cad0706c4972bad22223485c463bb2fd7418
Siddhartha Das <siddhartha.das.me@gmail.com>
2021-12-08 11:32:25 +0530

    checked in from vs

--- LearnGitAsp/Startup.cs
+++ LearnGitAsp/Startup.cs
@@ -17,7 +17,7 @@ namespace LearnGitAsp
         {
             Configuration = configuration;
         }
-
+        //new changes
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
