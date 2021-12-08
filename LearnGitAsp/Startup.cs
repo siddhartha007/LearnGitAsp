3a0b0af8d90833a48fd829bae0cbf6dada3ce655
Siddhartha Das <siddhartha.das.me@gmail.com>
2021-12-08 18:57:10 +0530

    is suo file checked in?

--- LearnGitAsp/Startup.cs
+++ LearnGitAsp/Startup.cs
@@ -18,6 +18,7 @@ namespace LearnGitAsp
             Configuration = configuration;
         }
         //new changes
+        //test diff
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
